0bc1f499aae7f6bf4e477c32563a226e2a4eaacd
luckypatcher471 <geoeljoseeparakadavil@gmail.com>
2026-02-25 00:44:46 +0530

    Refactor stt_vosk.py with model path placeholder
    Updated the Vosk speech recognition script to include a placeholder for the model path.

--- CAS-E/stt_vosk.py
+++ CAS-E/stt_vosk.py
@@ -6,7 +6,7 @@ import pygame
 
 pygame.mixer.init()
 
-model_path = r""
+model_path = r""#enter model path
 
 model = vosk.Model(model_path)  # Ensure you have the correct path to the Vosk model
 recognizer = vosk.KaldiRecognizer(model, 16000)
@@ -40,3 +40,4 @@ while True:
     
 
        
+
