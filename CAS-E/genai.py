7a3193fb31204a65f025ce8b96e8b8b2a201dac0
luckypatcher471 <geoeljoseeparakadavil@gmail.com>
2026-02-25 00:41:56 +0530

    Update genai.py

--- CAS-E/genai.py
+++ CAS-E/genai.py
@@ -1,7 +1,8 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyA_vGq9N5vFjpn4suG6Btcyy_yC2LsM-Ys")
+genai.configure(api_key="")
 
 for model in genai.list_models():
     if "generateContent" in model.supported_generation_methods:
+
         print(model.name)
